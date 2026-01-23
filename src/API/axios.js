04e09a82f774416f8489f035f8a52363ced1488d
Yidu-git <yman88.yt@gmail.com>
2026-01-23 19:55:58 +0300

    x

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const BASE_URL = "https://decomposed-unshepherding-vilma.ngrok-free.dev/";
 // const BASE_URL = "http://localhost:3000";
 // const BASE_URL = "https://snitchier-maeve-shufflingly.ngrok-free.dev";
+// s
 
 export const api = axios.create({
   baseURL: BASE_URL,
